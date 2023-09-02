823d288cf84b1c96f9fa432f9c1bc1c81f089d08
A7Coder <a7coder@gmail.com>
2023-09-02 07:23:02 +0530

    Products COntract and Test COmpleted

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+from scripts.deploy_products import deploy_products
+
+@pytest.fixture(scope="session", autouse=True)
+def getContract():
+    return deploy_products()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def dummyData():
+
+    return {
+           'a7coder@gmail.com': [{
+            'product_id': 'PROD1',
+            'product_name': 'F14',
+            'product_description': '5G Smarth Phone',
+            'product_category': 'Electronic',
+            'country_of_origin': 'India',
+            "date_of_expiry": '07/01/2040',
+            "date_of_manufacturing": '07/01/2020',
+            "price": 50000
+
+            },
+            {
+            'product_id': 'PROD2',
+            'product_name': 'APple',
+            'product_description': 'eatable',
+            'product_category': 'Food',
+            'country_of_origin': 'India',
+            "date_of_expiry": '07/01/2030',
+            "date_of_manufacturing": '07/01/2010',
+            "price": 70000
+
+            }],
+            'abc@gmail.com':[{
+            'product_id': 'TEST1',
+            'product_name': 'LG Fridge',
+            'product_description': 'Best Fridge in world',
+            'product_category': 'Electronic',
+            'country_of_origin': 'India',
+            "date_of_expiry": '07/01/2040',
+            "date_of_manufacturing": '07/01/2020',
+            "price": 100000
+
+            },
+            {
+            'product_id': 'Test2',
+            'product_name': 'FAN',
+            'product_description': 'Fastest Fan in World',
+            'product_category': 'Electronic',
+            'country_of_origin': 'India',
+            "date_of_expiry": '07/01/2010',
+            "date_of_manufacturing": '07/01/2070',
+            "price": 8000
+
+            }]
+
+    }
